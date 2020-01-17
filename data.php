89e5fb4d16de555a5fb5ec8c786d72adf108845f
Sergii Torgalo <serg.t0071@gmail.com>
2020-01-17 22:53:50 +0200

    task12

---
+++ data.php
@@ -0,0 +1,77 @@
+<?php
+
+return [
+    'mainMenu' => [
+        [
+            'title' => 'Home',
+            'url' => '#',
+        ],
+        [
+            'title' => 'About',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Work',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Services',
+            'url' => '#',
+        ],
+        [
+            'title' => 'Blog',
+            'url' => '#',
+        ],
+    ],
+    'siteLogo' => 'img/some.png',
+    'mainSlider' => [
+        [
+            'title' => [
+                'main' => 'Alecto',
+                'second' => 'is simple PSD Template',
+            ],
+            'description' => 'Nam varius accumsan elementum aliquam',
+            'link' => [
+                'text' => 'Learn more',
+                'url' => '#',
+            ],
+        ],
+        [
+            'title' => [
+                'main' => 'Alecto',
+                'second' => 'is simple PSD Template',
+            ],
+            'description' => 'Nam varius accumsan elementum aliquam',
+            'link' => [
+                'text' => 'Learn more',
+                'url' => '#',
+            ],
+        ],
+    ],
+
+    /*photo////////////////*/
+
+    'gallery' => [
+        [
+            'url' => 'assets/img/ze-1.jpg',
+            'alt' => 'ze-1',
+        ],
+        [
+            'url' => 'assets/img/ze-1.jpg',
+            'alt' => 'ze-1',
+        ],
+        [
+            'url' => 'assets/img/ze-1.jpg',
+            'alt' => 'ze-1',
+        ],
+        [
+            'url' => 'assets/img/ze-1.jpg',
+            'alt' => 'ze-1',
+        ],
+        [
+            'url' => 'assets/img/ze-1.jpg',
+            'alt' => 'ze-1',
+        ],
+    ],
+
+];
